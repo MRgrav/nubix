8dd1caab8e32ebbb21054b98b27a99c0fbabf9e0
Ankit-Gupta-pixel <60318303+Ankit-Gupta-pixel@users.noreply.github.com>
2026-02-13 14:00:13 +0530

    update to student stream detail api (#20)

--- routes/studentStreamRoutes.js
+++ routes/studentStreamRoutes.js
@@ -1,13 +1,27 @@
-import express from 'express';
-import { enrollStudentInStream, getStudentStreams, getStudentStream, updateStudentStream, unenrollStudent } from '../controllers/studentStreamController.js';
-import { authenticate, authorize } from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  enrollStudentInStream,
+  getStudentStreams,
+  getStudentStream,
+  updateStudentStream,
+  unenrollStudent,
+  getMyEnrollment,
+} from "../controllers/studentStreamController.js";
+import { authenticate, authorize } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/enroll', authenticate, authorize('ADMIN', 'STAFF'), enrollStudentInStream);
-router.get('/', authenticate, getStudentStreams);
-router.get('/:id', authenticate, getStudentStream);
-router.put('/:id', authenticate, authorize('ADMIN'), updateStudentStream);
-router.delete('/:id', authenticate, authorize('ADMIN'), unenrollStudent);
+router.post(
+  "/enroll",
+  authenticate,
+  authorize("ADMIN", "STAFF"),
+  enrollStudentInStream,
+);
+router.get("/", authenticate, authorize("ADMIN", "STAFF"), getStudentStreams);
+router.get("/my-enrollment", authenticate, getMyEnrollment);
+
+router.get("/:id", authenticate, authorize("ADMIN", "STAFF"), getStudentStream);
+router.put("/:id", authenticate, authorize("ADMIN"), updateStudentStream);
+router.delete("/:id", authenticate, authorize("ADMIN"), unenrollStudent);
 
 export default router;
